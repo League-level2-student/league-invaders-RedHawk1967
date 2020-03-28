ea91869e5975b635f1ecbd88f2f71c4bfffe9f5f
Jack <jackgorton57@gmail.com>
2020-03-28 11:41:06 -0700

    ?

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,43 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	
+	public static void main(String[] args) {
+		
+	}
+
+
+	JFrame frame;
+
+
+	public LeagueInvaders() {
+		
+frame = new JFrame();
+	}
+
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	
+LeagueInvaders con = new LeagueInvaders();
+
+
+
+public void setup() {
+
+	
+	
+	
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 }
+
+
+
+
+
+}
+
+
+
+
+
---
+++ src/leagueinvadersrunner.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+
+public class leagueinvadersrunner {
+
+public static void main(String[] args) {
+	
+}
+
+
+JFrame frame;
+
+
+public leagueInvaders() {
+	
+
+}
+
+public static final int WIDTH = 500;
+public static final int HEIGHT = 800;
+}
