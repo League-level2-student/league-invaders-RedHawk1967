c68d1f893e2364a5f23a3a5e1745e9c8005f9914
Jack <jackgorton57@gmail.com>
2020-04-03 16:39:44 -0700

    onto control p1

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -5,13 +5,14 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel{
-
+ Font smallFont;
  Font titleFont;
 
  public GamePanel() {
 	    titleFont = new Font("Arial", Font.PLAIN, 48);
-}
- 
+	    smallFont = new Font("Arial", Font.PLAIN, 25);
+ }
+int enemies; 
 	@Override
 	public void paintComponent(Graphics g){
 		if(currentState == MENU){
@@ -38,12 +39,24 @@ void drawMenuState(Graphics g) {g.setColor(Color.BLUE);
 g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT); 
 g.setFont(titleFont);
 g.setColor(Color.WHITE);
-g.drawString("text", y, x);
+g.drawString("League Invaders",50, 100);g.setFont(smallFont);
+g.drawString("Press Enter To Start", 100 , 400);
+g.drawString("Press Space for instructions", 75, 700);
 }
+
 void drawGameState(Graphics g) { g.setColor(Color.BLACK);
-g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT); }
+g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT); 
+}
 void drawEndState(Graphics g)  { g.setColor(Color.RED);
-g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT); }
+g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT); 
+g.setFont(titleFont);
+
+g.drawString("GAME OVER", 350,400);
+g.setFont(smallFont);
+g.drawString("You killed " + enemies + " enemies", 250,400);
+g.drawString("Press Enter to restart", 75,700 );
+
+}
 
 
 
